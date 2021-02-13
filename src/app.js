7d16c9326070233b80814b60f13ce16297937b73
Dinkymoo <daniellefunnygraan@gmail.com>
2021-02-13 12:13:29 +0100

    Add initial vue app component with template

--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
 import Vue from 'vue'
+
+Vue.component('app', {
+  template: `
+      <div id="app">
+        <nav class="nav has-shadow">
+          <div class="container">
+            <a href="/">
+              <img src="https://static.tvmaze.com/images/tvm-header-logo.png"
+                alt="TV Maze" />
+            </a>
+          </div>
+        </nav>
+        <section class="main-section section"></section>
+        <footer class="footer">
+          <div class="container">
+            <div class="content has-text-centered">
+              Follow us on
+              <a href="http://api.tvmaze.com"
+              target="_blank">Maze Apis</a>
+            </div>
+          </div>
+        </footer>
+      </div>
+    `
+})
 const app = new Vue({
-  data: { hello: 'Hello ABN AMRO' },
-  template: `<div id="app">{{hello}}</div>`
+  render: h => h('app')
 })
 export { app }
