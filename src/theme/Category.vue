4ea4d83c619cfc3862fe0f50f8e9a239dd284b85
Danielle <danielletulp@yahoo.com>
2021-02-15 18:34:45 +0100

    Add category component

---
+++ src/theme/Category.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="columns">
+<button class="button  category">Drama</button>
+<button class="button  category">Drama</button>
+<button class="button category">Science-Fiction</button>
+<button class="button category">Thriller</button>
+<button class="button category">Horror</button>
+
+</div>
+</template>
+<script>
+
+ import {mapGetters} from 'vuex';
+  export default {
+  computed: {
+      ... mapGetters(['genre'])
+  }
+}
+
+</script>
+<style scoped>
+.category {
+  background-color: blue;
+  margin: 10px;
+  padding:10px;
+  color: white;
+
+}
+</style>
+
+
