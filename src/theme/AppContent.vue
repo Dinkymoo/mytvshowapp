70c7b8e8a74f8f49e3398a4b7c032ef46ccad5ee
Danielle <danielletulp@yahoo.com>
2021-02-15 18:38:05 +0100

    Fix router import

--- src/theme/AppContent.vue
+++ src/theme/AppContent.vue
@@ -1,34 +1,60 @@
 <template>
   <section class="main-section section">
-    <div class="columns " v-for="(show) in shows" v-bind:key="show.id">
-        <show class="column">
-          <img slot="image" v-bind:src="show.image.medium"
-                alt="TV Maze" class="card-image" v-show="show.image.medium" />
-          <p slot="title" class="title is-4">{{show.name}}</p>
-          <p slot="language" class="subtitle is-6">{{show.language}}</p>
+    <div></div>
+    <search class="columns"></search>
+    <div></div>
+    <category class="columns"></category>
+    <div></div>
+    <div class="columns" v-for="(show) in shows" v-bind:key="show.id">
+      <show class="column" v-if="show.show">
+          <img v-if="show.show.image" slot="image" v-bind:src="show.show.image.medium"
+                alt="TV Maze" class="card-image" />
+          <router-link :to="{name: 'details', params: {id: show.id}}" slot="title" class="title is-4" target="_blank">
+          {{show.show.name}}
+          </router-link>
         </show>
+        <show class="column" v-else>
+          <img v-if="show.image" slot="image" v-bind:src="show.image.medium"
+                alt="TV Maze" class="card-image" />
+          <router-link :to="{name: 'details', params: {id: show.id}}"  slot="title" class="title is-4" target="_blank">
+          {{show.name}}
+          </router-link>
+        </show>
+
     </div>
   </section>
 </template>
 <script>
-
 import Show from './Show.vue';
-import appService from '../app.service';
+import Search from './Search.vue';
+import Details from './Details.vue';
+import Category from './Category.vue';
+import {mapGetters} from 'vuex'
+import {mapActions} from 'vuex'
+
 export default {
    components: {
      'show': Show,
+      'search': Search,
+      'details': Details,
+      'category': Category
+   },
+    computed: {
+    ... mapGetters(['shows'])
    },
     data() {
       return {
-        shows: [],
         id: this.$route.params.id
         }
     },
     methods: {
-        loadShows() {
-        console.log(this.$route.params.id)
-        //data can be different here based on id
-        appService.getShows().then(data => this.shows = data)
+      ... mapActions({
+      updateshows : 'updateshows'}),
+      loadShows() {
+          this.$store.dispatch('updateshows', undefined)
+      },
+      showDetails() {
+        console.log('xxxxxxxxxxxxxx')
       }
     },
     created()  {
@@ -46,5 +72,9 @@ export default {
     }
   }
 </script>
+
 <style lang="scss" scoped>
+.card-image {
+  max-width: 75%;
+}
 </style>
