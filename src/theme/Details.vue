046393b74ce83a62e96e946b32e69654c0a8f801
Danielle <danielletulp@yahoo.com>
2021-02-15 18:29:58 +0100

    Add details component

---
+++ src/theme/Details.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="card">
+<div class="card-content">
+    <div class="media">
+      <div class="media-left">
+           <img v-bind:src="show.image.medium"
+                alt="TV Maze" class="nav-item is-tab" />
+      </div>
+      <div v-html="show.name" class="media-content">
+        {{show.name}}
+      </div>
+      <div v-html="show.summary"  class="media-content">
+        {{show.summary}}
+      </div>
+    </div>
+</div>
+</div>
+</template>
+
+<script>
+ import appService from './../app.service'
+export default {
+  data() {
+      return {
+        id: this.$route.params.id,
+        show: undefined
+        }
+    },
+  created() {
+    appService.showDetails(this.id).then(result => {
+      this.show = result
+    }
+
+    )
+  }
+
+}
+</script>
