dc60426c6e261a9e7e322ea43499385fe2f23135
Danielle <danielletulp@yahoo.com>
2021-02-15 18:29:44 +0100

    Add search component

---
+++ src/theme/Search.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="columns">
+      <input v-model="searchTerm" class="input column" type="text"
+			  placeholder="Search Term" />
+      <button v-on:click="searchShows()" class="button is-primary column is-one-third">Search</button>
+    </div>
+</template>
+<script>
+import {mapGetters} from 'vuex'
+import {mapActions} from 'vuex'
+export default {
+  data(){
+     return {
+    searchTerm: ''
+  }},
+  computed: {
+    ... mapGetters(['shows'])
+  },
+
+  methods: {
+     ... mapActions({
+    updateshows : 'updateshows'}),
+    searchShows() {
+        this.$store.dispatch('updateshows', this.searchTerm)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped >
+
+</style>
