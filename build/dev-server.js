24465e515dc584f964c08680a4489047c0eebca2
Danielle <danielletulp@yahoo.com>
2021-02-18 08:38:13 +0100

    Format build files

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,21 +1,21 @@
-const webpack = require('webpack');
-const clientConfig = require('./webpack.client.config');
-module.exports = function setupDevServer(app) {
+const webpack = require('webpack')
+const clientConfig = require('./webpack.client.config')
+module.exports = function setupDevServer (app) {
   clientConfig.entry.app = [
     'webpack-hot-middleware/client',
-    clientConfig.entry.app,
-  ];
+    clientConfig.entry.app
+  ]
   clientConfig.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
-  );
-  const clientCompliler = webpack(clientConfig);
+  )
+  const clientCompliler = webpack(clientConfig)
   app.use(
     require('webpack-dev-middleware')(clientCompliler, {
       stats: {
-        colors: true,
-      },
+        colors: true
+      }
     })
-  );
-  app.use(require('webpack-hot-middleware')(clientCompliler));
-};
+  )
+  app.use(require('webpack-hot-middleware')(clientCompliler))
+}
--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,8 +1,8 @@
-const path = require('path');
+const path = require('path')
 
 const config = {
   entry: {
-    app: path.resolve(__dirname, '../src/client-entry.js'),
+    app: path.resolve(__dirname, '../src/client-entry.js')
   },
   module: {
     rules: [
@@ -10,28 +10,28 @@ const config = {
         enforce: 'pre',
         test: '/(\.js$)/|/(\.vue$)/',
         loader: 'eslint-loader',
-        exclude: '/node-modules/',
+        exclude: '/node-modules/'
       },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
           css: 'css-loader',
-        'scss': 'sass-loader',
-        extractCSS : true
-        },
+          'scss': 'sass-loader',
+          extractCSS: true
+        }
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: '/node-modules/'
-      },
-    ],
+      }
+    ]
   },
-    output: {
-      path: path.resolve(__dirname, '../dist'),
-      publicPath: '/',
-      filename: 'assets/js/[name].js',
-    },
-};
-module.exports = config;
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
+    filename: 'assets/js/[name].js'
+  }
+}
+module.exports = config
